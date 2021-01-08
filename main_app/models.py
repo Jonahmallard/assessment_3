351d018651d37753c0ece7fea59a5f5de0adc14c
Jonah Mallard <benjaminjmallard@gmail.com>
2021-01-08 13:21:02 -0600

    Add Model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+class Widget(models.Model):
+    description = models.TextField(max_length=250, blank=True, null=True)
+    quantity = models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.item
+
+    def get_absolute_url(self):
+        return reverse('index')
