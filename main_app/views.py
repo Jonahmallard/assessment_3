220ae22051963cc776700829783eed85b30d0997
Jonah Mallard <benjaminjmallard@gmail.com>
2021-01-08 14:55:07 -0600

    Add Widget

--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Widget
+from django.views.generic.edit import CreateView
+from .forms import WidgetForm
 
 def index(request):
     widgets = Widget.objects.all()
     return render(request, 'index.html', {'widgets': widgets})
+
+class WidgetCreate(CreateView):
+    model = Widget
+    fields = "__all__"
+
+def delete_widget(request, widget_id):
+    Widget.objects.get(id=widget_id).delete()
+    return redirect('/')
